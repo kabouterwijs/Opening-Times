f63720ada808c5a1f633576e4d8e1c4617d9c924
Julian Burgess <julian@opening-times.co.uk>
2009-10-23 23:57:00 +0100

    Fixed rendering when facility has been retired

--- app/controllers/slug_trap_controller.rb
+++ app/controllers/slug_trap_controller.rb
@@ -6,7 +6,7 @@ class SlugTrapController < ApplicationController
 
     return redirect_slug_or_404(params[:slug]) unless @facility
 
-    render 'facilities/_retired' and return if @facility.retired?
+    render 'facilities/retired' and return if @facility.retired?
 
     @multi = params[:multi] || "0"
     @multi = @multi.split(",").map(&:to_i)
